47f8d3c906bdc4aa7515581cf4c79f3e19032c35
yk8710 <y.kaneko198710@gmail.com>
2022-06-19 14:51:31 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,63 @@
+class PrototypesController < ApplicationController
+  before_action :move_to_index, only: :edit
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    # @prototypesave = Prototype.create(prototype_params)
+    if @prototype.save
+      redirect_to root_path(@prototype)
+    else
+      # @prototype = Prototype.new(prototype_params)
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    prototype.update(prototype_params)
+    if
+      redirect_to root_path(@prototype)
+    else
+      prototype = Prototype.includes(:user)
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+end
+
